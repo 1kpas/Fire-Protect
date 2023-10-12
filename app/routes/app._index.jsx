9d1d462a22bebe905169b3e4397831f70cd3a046
1kpas <ldozila@gmail.com>
2023-10-12 18:44:26 -0300

    Teste de sucesso no pagamento

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,13 +1,13 @@
-import { LoaderArgs } from "@remix-run/node";
 import { authenticate, MONTHLY_PLAN } from "../shopify.server";
+import { redirect } from "@remix-run/node";
 
-export const loader = async ({ request }: LoaderArgs) => {
+export const loader = async ({ request }) => {
   const { billing } = await authenticate.admin(request);
   await billing.require({
     plans: [MONTHLY_PLAN],
     isTest: true,
-    onFailure: async () => billing.request({ plan: MONTHLY_PLAN }),
+    onFailure: async () => billing.request({ plan: MONTHLY_PLAN })
   });
 
-  // App logic
+  return redirect("/home");
 };
