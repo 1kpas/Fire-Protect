e79e88b1602d3b213cf23d030bd99de2b8a17d0d
1kpas <ldozila@gmail.com>
2023-10-12 20:14:34 -0300

    Correção nas urls

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -17,7 +17,7 @@ export const apiShopify = shopifyApi({
   apiSecretKey: "ba72f01fdf360010b972de17565c2643" || "",
   apiVersion: LATEST_API_VERSION,
   scopes: ["read_script_tags", "write_script_tags"],
-  hostName: "https://viperprotect.store",
+  hostName: "https://vipertesteblinder.myshopify.com",
   isEmbeddedApp: false,
   restResources
 })
@@ -42,7 +42,7 @@ const shopify = shopifyApp({
   apiSecretKey: "ba72f01fdf360010b972de17565c2643",
   apiVersion: LATEST_API_VERSION,
   scopes: ["read_script_tags", "write_script_tags"],
-  appUrl: "https://viperprotect.store",
+  appUrl: "https://vipertesteblinder.myshopify.com",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
