cc683f47d98cbae455328403f8cce60210e35091
1kpas <ldozila@gmail.com>
2023-10-12 19:21:04 -0300

    Validação de pagamento

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,15 +1,13 @@
+import { redirect } from "@remix-run/node";
 import { authenticate, MONTHLY_PLAN } from "../shopify.server";
-import Index from "./home";
 
 export const loader2 = async ({ request }) => {
   const { billing } = await authenticate.admin(request);
-  const billingCheck = await billing.require({
+  await billing.require({
     plans: [MONTHLY_PLAN],
     isTest: true,
     onFailure: async () => billing.request({ plan: MONTHLY_PLAN })
   });
 
-  if(billingCheck){
-    <Index/>
-  }
+  return redirect('/home')
 };
