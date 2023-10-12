5b3b48f8c5ce12724b13daac4f27a626ac353d7c
1kpas <ldozila@gmail.com>
2023-10-12 19:50:40 -0300

    billingCheck

--- app/routes/app._index.ts
+++ app/routes/app._index.ts
@@ -1,16 +1,15 @@
-import { LoaderArgs } from "@remix-run/node";
+import { LoaderArgs, redirect } from "@remix-run/node";
 import { authenticate, MONTHLY_PLAN } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderArgs) => {
   const { billing } = await authenticate.admin(request);
-  await billing.require({
+  const billingCheck = await billing.require({
     plans: [MONTHLY_PLAN],
-    onFailure: async () => billing.request({
-      plan: MONTHLY_PLAN,
-      isTest: true,
-      returnUrl: '/app/teste',
-    }),
+    isTest: true,
+    onFailure: async () => billing.request({ plan: MONTHLY_PLAN }),
   });
 
-  // App logic
+  if(billingCheck){
+    redirect('/app/home')
+  }
 };
