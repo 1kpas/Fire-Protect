60c71b9843c798c964ff73751b2340e8cb2f63e3
1kpas <ldozila@gmail.com>
2023-10-12 18:11:30 -0300

    Teste billing

---
+++ app/routes/app._index.ts
@@ -0,0 +1,13 @@
+import { LoaderArgs } from "@remix-run/node";
+import { authenticate, MONTHLY_PLAN } from "../shopify.server";
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const { billing } = await authenticate.admin(request);
+  await billing.require({
+    plans: [MONTHLY_PLAN],
+    isTest: true,
+    onFailure: async () => billing.request({ plan: MONTHLY_PLAN }),
+  });
+
+  // App logic
+};
