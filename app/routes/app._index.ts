e0d4d95baed263755173fcfea1644b5791bfc056
1kpas <ldozila@gmail.com>
2023-10-12 19:53:20 -0300

    return redirect

--- app/routes/app._index.ts
+++ app/routes/app._index.ts
@@ -3,13 +3,11 @@ import { authenticate, MONTHLY_PLAN } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderArgs) => {
   const { billing } = await authenticate.admin(request);
-  const billingCheck = await billing.require({
+  await billing.require({
     plans: [MONTHLY_PLAN],
     isTest: true,
     onFailure: async () => billing.request({ plan: MONTHLY_PLAN }),
   });
 
-  if(billingCheck){
-    redirect('/app/home')
-  }
+  return redirect('/app/home')
 };
