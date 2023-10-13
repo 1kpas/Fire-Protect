096cddeba2c1ebbb4e5b25b462602dbf5c4f2e35
1kpas <ldozila@gmail.com>
2023-10-13 01:28:19 -0300

    Voltando como estava

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,19 +1,20 @@
-import { json, redirect } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
 import { AppProvider } from "@shopify/shopify-app-remix/react";
 import { boundary } from "@shopify/shopify-app-remix/server";
 import st from "./_index/style.css";
 
-import { ANNUAL_PLAN, MONTHLY_PLAN, authenticate } from "../shopify.server";
+import { MONTHLY_PLAN, authenticate } from "../shopify.server";
 
 export const links = () => [{ rel: "stylesheet", href: st }];
 
 export async function loader({ request }) {
   const { billing } = await authenticate.admin(request);
+  
   await billing.require({
     plans: [MONTHLY_PLAN],
     isTest: true,
-    onFailure: async () => redirect('/select-plan'),
+    onFailure: async () => billing.request({ plan: MONTHLY_PLAN }),
   });
 
   return json({ apiKey: process.env.SHOPIFY_API_KEY });
