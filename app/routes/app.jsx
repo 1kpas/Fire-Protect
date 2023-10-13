6fb3fcc82792697acee26a9e3d18d004b18eac69
1kpas <ldozila@gmail.com>
2023-10-13 01:57:12 -0300

    Voltar como estava

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,28 +1,23 @@
-import { json, redirect } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
 import { AppProvider } from "@shopify/shopify-app-remix/react";
 import { boundary } from "@shopify/shopify-app-remix/server";
 import st from "./_index/style.css";
 
-import { ANNUAL_PLAN, MONTHLY_PLAN, authenticate } from "../shopify.server";
+import { MONTHLY_PLAN, authenticate } from "../shopify.server";
 
 export const links = () => [{ rel: "stylesheet", href: st }];
 
 export async function loader({ request }) {
   const { billing } = await authenticate.admin(request);
-  const billingCheck = await billing.require({
-    plans: [MONTHLY_PLAN, ANNUAL_PLAN],
+
+  await billing.require({
+    plans: [MONTHLY_PLAN],
     isTest: true,
-    onFailure: async () => redirect('/app'),
+    onFailure: async () => billing.request({ plan: MONTHLY_PLAN }),
   });
 
-  if(billingCheck.hasActivePayment){
-    redirect('/app/panel')
-    return json({ apiKey: process.env.SHOPIFY_API_KEY });
-  }
-
-  return null;
-
+  return json({ apiKey: process.env.SHOPIFY_API_KEY });
 }
 
 export default function App() {
@@ -33,9 +28,6 @@ export default function App() {
       <ui-nav-menu>
         <Link to="/app" rel="home">
           Home
-        </Link>
-        <Link to="/app/panel" rel="panel">
-          Painel de Controle
         </Link>
         <Link to="/app/config" rel="config">
           Configurações
