3f1dd7e8324f39571d7132abd532dbfa2d206027
1kpas <ldozila@gmail.com>
2023-10-13 00:52:18 -0300

    Correção na página config

--- app/routes/app.config.jsx
+++ app/routes/app.config.jsx
@@ -1,9 +1,3 @@
-import { authenticate } from "~/shopify.server";
-
-export const loader = async ({ request }) => {
-    await authenticate.admin(request);
-  };
-
 export default function Config(){
-    <div style={{width: 200, height: 200, background: 'red'}}></div>
+    return (<div style={{width: 200, height: 200, background: 'red'}}></div>)
 }
