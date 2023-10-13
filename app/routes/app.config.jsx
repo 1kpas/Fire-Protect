3a396d6a3198fc9eb0884e77b5c7293b4d5b9ff8
1kpas <ldozila@gmail.com>
2023-10-13 00:48:19 -0300

    Autenticacao pra config

--- app/routes/app.config.jsx
+++ app/routes/app.config.jsx
@@ -1,3 +1,9 @@
+import { authenticate } from "~/shopify.server";
+
+export const loader = async ({ request }) => {
+    await authenticate.admin(request);
+  };
+
 export default function Config(){
     <div style={{width: 200, height: 200, background: 'red'}}></div>
 }
