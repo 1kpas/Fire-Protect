f99a390d3637b18dd3f1e86cd99810c87948ad34
1kpas <ldozila@gmail.com>
2023-10-12 21:42:32 -0300

    Testando

--- app/routes/app._index.ts
+++ app/routes/app._index.ts
@@ -3,13 +3,11 @@ import { authenticate, MONTHLY_PLAN } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderArgs) => {
   const { billing } = await authenticate.admin(request);
-  const hasPayment = await billing.require({
+  await billing.require({
     plans: [MONTHLY_PLAN],
-    onFailure: async () => billing.request({ plan: MONTHLY_PLAN }),
+    onFailure: () => billing.request({ plan: MONTHLY_PLAN }),
     isTest: true,
   });
 
-  if(hasPayment){
-    return redirect('/app/testando')
-  }
+  return redirect('/app/testando')
 };
