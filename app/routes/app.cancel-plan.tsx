245a4ce9d45c8e19b348929e9268fa28b45ef4de
1kpas <ldozila@gmail.com>
2023-10-13 21:13:09 -0300

    Teste cancelar 1

--- app/routes/app.cancel-plan.tsx
+++ app/routes/app.cancel-plan.tsx
@@ -1,22 +1,3 @@
-import { LoaderArgs } from "@remix-run/node";
-import { authenticate, MONTHLY_PLAN } from "../shopify.server";
-
-export const loader = async ({ request }: LoaderArgs) => {
-  const { billing } = await authenticate.admin(request);
-  const billingCheck = await billing.require({
-    plans: [MONTHLY_PLAN],
-    onFailure: async () => billing.request({ plan: MONTHLY_PLAN }),
-  });
-
-  const subscription = billingCheck.appSubscriptions[0];
-  await billing.cancel({
-    subscriptionId: subscription.id,
-    isTest: true,
-    prorate: true,
-   });
-};
-
-
 export default function CancelPlan(){
     return (<div>
         <h2>Plano cancelado com sucesso!</h2>
